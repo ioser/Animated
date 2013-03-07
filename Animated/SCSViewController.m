48da3b5a8026955c41412322cb557bb173a87e55
Richard Millet <milletdesigns@me.com>
2013-03-06 22:46:07 -0800

    First commit.

--- Animated/SCSViewController.m
+++ Animated/SCSViewController.m
@@ -12,7 +12,9 @@
 
 @end
 
-@implementation SCSViewController
+@implementation SCSViewController {
+	bool changeColor;
+}
 
 - (void)viewDidLoad
 {
@@ -24,6 +26,47 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - My Added Methods
+
+- (IBAction)showOffMore:(UITapGestureRecognizer *)sender {
+	
+	[UIView animateWithDuration:2
+					 animations:^{
+						 self.view.backgroundColor = [UIColor greenColor];
+						 self.view.transform = CGAffineTransformMakeScale(0.2, 0.1);
+					 }
+					 completion:^(BOOL finished) {
+						 self.view.backgroundColor = [UIColor redColor];
+						 self.view.transform = CGAffineTransformIdentity;
+					 }];
+}
+
+- (IBAction)showOff:(UITapGestureRecognizer *)sender {
+	UIColor *color;
+	CGFloat sx;
+	CGFloat sy;
+	
+	if (!changeColor) {
+		changeColor = true;
+		color = [UIColor greenColor];
+		sx = 0.2;
+		sy = 0.2;
+	} else {
+		changeColor = false;
+		color = [UIColor redColor];
+		sx = 1;
+		sy = 1;
+	}
+	
+	[UIView animateWithDuration:2 animations:^{
+		self.view.backgroundColor = color;
+		self.view.transform = CGAffineTransformMakeScale(sx, sy);		
+	}];
+	
+//	self.view.backgroundColor = color;
+//	self.view.transform = CGAffineTransformIdentity;
 }
 
 @end
